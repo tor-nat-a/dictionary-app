7dfb64119a9cfe8a85744a284d8d824985a92b7c
tor-nat-a <nattorhon@gmail.com>
2022-09-18 16:25:46 +0300

    Added API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,22 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
 
+    function handleResponse(response) {
+        console.log(response.data[0]);
+        }
+
     function search(event) {
         event.preventDefault();
         alert("Searching");
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        console.log(apiUrl);
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleKeywordChange(event) {
